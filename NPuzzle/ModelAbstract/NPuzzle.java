7c4aa5b6caeb068af68f89ebc5f275b2d57cdd5c
MorrisZJ <91582604+MorrisZJ@users.noreply.github.com>
2022-10-07 20:38:33 -0400

    First draft, A-star finish.

--- NPuzzle/ModelAbstract/NPuzzle.java
+++ NPuzzle/ModelAbstract/NPuzzle.java
@@ -3,25 +3,26 @@ package NPuzzle.ModelAbstract;
 import java.util.Arrays;
 
 /**
- * Class stores the array representation of the states.
+ * Class stores the array representation of the states of N-Puzzle.
  * @param <representationType> Type of array.
+ * @author Jiamu Zhang
  */
 public abstract class NPuzzle<representationType> {
 
     /**
-     *
+     * The size of the puzzle.
      */
     private final int puzzleSize;
 
     /**
-     *
+     * The array representation of the state.
      */
     private final representationType[] puzzleStateArray;
 
     /**
-     *
-     * @param puzzleStateArray
-     * @param puzzleSize
+     * The constructor of the NPuzzle.
+     * @param puzzleStateArray The array representation of the state.
+     * @param puzzleSize The size of the puzzle.
      */
     protected NPuzzle(representationType[] puzzleStateArray, int puzzleSize) {
         this.puzzleStateArray = puzzleStateArray;
@@ -29,31 +30,31 @@ public abstract class NPuzzle<representationType> {
     }
 
     /**
-     *
-     * @return
+     * A getter method that return the state of puzzle.
+     * @return Return the array representation of state.
      */
     public representationType[] getPuzzleStateArray() {
         return puzzleStateArray;
     }
 
     /**
-     *
-     * @return
+     * A getter method that return the size of the puzzle.
+     * @return Return the size of the puzzle.
      */
     public int getPuzzleSize() {
         return puzzleSize;
     }
 
     /**
-     *
-     * @param nPuzzle
-     * @return
+     * An equal method tha used to compare to another state whether two states are the same.
+     * @param nPuzzle The puzzle state.
+     * @return Return true if equal, else return false.
      */
     public abstract boolean equals(NPuzzle<representationType> nPuzzle);
 
     /**
-     *
-     * @return
+     * An override toString method that used to print the state representation to the terminal.
+     * @return Return string representation of puzzle.
      */
     @Override
     public String toString() {
